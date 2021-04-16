d2f9d1e4aa55eb91dff7415e6d9784fd1fd6e083
akhil-rana <akhilrana0001@gmail.com>
2021-04-16 20:49:22 +0530

    ADD: hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.scss';
 import Header from './components/header';
 import ConnectPage from './containers/connect-page';
@@ -8,13 +8,13 @@ function App() {
   return (
     <div className='App'>
       <Header></Header>
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path='/connect'>
             <ConnectPage />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
