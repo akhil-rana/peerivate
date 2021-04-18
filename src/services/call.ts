e0d18a9f4a5fcded8c259b558621a1199f82d17c
akhil-rana <akhilrana0001@gmail.com>
2021-04-18 21:44:27 +0530

    REMOVED: call.ts

--- src/services/call.ts
+++ src/services/call.ts
@@ -1,44 +1,43 @@
-import Peer from 'peerjs';
-
-// const peer = new Peer();
-
 export async function callPeer(peerId: string, peer: any) {
-  //   await (navigator.mediaDevices as any).getDisplayMedia(
-  //     { video: true },
-  //     (stream: any) => {
-  //       const call = peer.call(peerId, stream);
-  //       console.log('calling peer: ' + peerId);
-  //       call.on('stream', (remoteStream) => {
-  //         console.log(typeof stream);
-  //         // Show stream in some <video> element.
-  //       });
-  //     },
-  //     (err: any) => {
-  //       console.error('Failed to get local stream', err);
-  //     }
-  //   ).catch((err:any) =>{
-  //       console.error(err)
-  //   });
-  const conn = peer.connect(peerId);
-  conn.on('open', () => {
-    conn.send('hi!');
+  const mediaDevices = navigator.mediaDevices as any;
+  // const stream = await mediaDevices.getDisplayMedia({ video: true });
+  const stream = await mediaDevices.getUserMedia({ audio: true });
+
+  const call = peer.call(peerId, stream);
+  console.log('calling peer: ' + peerId);
+  call.on('stream', (remoteStream: any) => {
+    console.log(typeof stream);
+    // Show stream in some <video> element.
   });
 }
 
+//testing with simple message
+// const conn = peer.connect(peerId);
+// conn.on('open', () => {
+//   console.log('connect to ' + peerId);
+//   conn.send('hi!');
+// });
+
 export async function answerPeer(peer: any) {
-  peer.on('call', (call: any) => {
-    console.log('hey');
-    (navigator.mediaDevices as any).getDisplayMedia(
-      { video: true },
-      (stream: any) => {
-        call.answer(stream); // Answer the call with an A/V stream.
-        call.on('stream', (remoteStream: any) => {
-          // Show stream in some <video> element.
-        });
-      },
-      (err: any) => {
-        console.error('Failed to get local stream', err);
-      }
-    );
+  peer.on('call', async (call: any) => {
+    const mediaDevices = navigator.mediaDevices as any;
+    const stream = await mediaDevices.getUserMedia({ audio: true });
+    // const stream = await mediaDevices.getDisplayMedia({ video: true });
+
+    call.answer(stream); // Answer the call with an A/V stream.
+    call.on('stream', (remoteStream: any) => {
+      // Show stream in some <video> element.
+    });
+
+    //testing with simple message
+    // peer.on('connection', (conn: any) => {
+    //   conn.on('data', (data:any) => {
+    //     // Will print 'hi!'
+    //     console.log(data);
+    //   });
+    //   conn.on('open', () => {
+    //     conn.send('hello!');
+    //   });
+    // });
   });
 }
