27e33fbac3e10fc37e375a6dbb0b6a4bd7046bd7
akhil-rana <akhilrana0001@gmail.com>
2021-07-24 21:26:31 +0530

    CHANGE: turn server to xirsys

--- src/containers/connect-page/index.tsx
+++ src/containers/connect-page/index.tsx
@@ -35,7 +35,14 @@ function ConnectPage(props: any) {
       {
         username: process.env.REACT_APP_TURN_SERVER_USERNAME || '',
         credential: process.env.REACT_APP_TURN_SERVER_PASSWORD || '',
-        urls: ['turn:numb.viagenie.ca'],
+        urls: [
+          'turn:bn-turn1.xirsys.com:80?transport=udp',
+          'turn:bn-turn1.xirsys.com:3478?transport=udp',
+          'turn:bn-turn1.xirsys.com:80?transport=tcp',
+          'turn:bn-turn1.xirsys.com:3478?transport=tcp',
+          'turns:bn-turn1.xirsys.com:443?transport=tcp',
+          'turns:bn-turn1.xirsys.com:5349?transport=tcp',
+        ],
       },
     ],
   };
