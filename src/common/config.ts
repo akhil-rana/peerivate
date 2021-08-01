b747e5e244342e15c13ded3796ad2d952c407460
akhil-rana <akhilrana0001@gmail.com>
2021-08-02 01:15:29 +0530

    ADD: own turn server

--- src/common/config.ts
+++ src/common/config.ts
@@ -6,16 +6,14 @@ export const config = {
       urls: csv_to_array(process.env.REACT_APP_STUN_SERVER_URL_LIST_CSV || ''),
     },
     {
+      username: 'turn@akhilrana.com',
+      credential: 'turnpassword',
+      urls: ['turn:numb.viagenie.ca'],
+    },
+    {
       username: process.env.REACT_APP_TURN_SERVER_USERNAME || '',
       credential: process.env.REACT_APP_TURN_SERVER_PASSWORD || '',
-      urls: [
-        'turn:bn-turn1.xirsys.com:80?transport=udp',
-        'turn:bn-turn1.xirsys.com:3478?transport=udp',
-        'turn:bn-turn1.xirsys.com:80?transport=tcp',
-        'turn:bn-turn1.xirsys.com:3478?transport=tcp',
-        'turns:bn-turn1.xirsys.com:443?transport=tcp',
-        'turns:bn-turn1.xirsys.com:5349?transport=tcp',
-      ],
+      urls: ['turn:vps.akhilrana.com'],
     },
   ],
 };
