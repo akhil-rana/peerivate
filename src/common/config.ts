f70f2cc4a50511e4cfabdf9c18fdce1c82f6af60
Akhil Rana <akhilrana0001@gmail.com>
2022-03-16 02:59:02 +0530

    Update config.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -20,18 +20,18 @@ export const config = {
       username: "openrelayproject",
       credential: "openrelayproject"
     },
-//     {
-//       urls: csv_to_array(process.env.REACT_APP_STUN_SERVER_URL_LIST_CSV || ''),
-//     },
-//     {
-//       username: 'turn@akhilrana.com',
-//       credential: 'turnpassword',
-//       urls: ['turn:numb.viagenie.ca'],
-//     },
-//     {
-//       username: process.env.REACT_APP_TURN_SERVER_USERNAME || '',
-//       credential: process.env.REACT_APP_TURN_SERVER_PASSWORD || '',
-//       urls: ['turn:vps.akhilrana.com'],
-//     },
+    {
+      urls: csv_to_array(process.env.REACT_APP_STUN_SERVER_URL_LIST_CSV || ''),
+    },
+    {
+      username: 'turn@akhilrana.com',
+      credential: 'turnpassword',
+      urls: ['turn:numb.viagenie.ca'],
+    },
+    {
+      username: process.env.REACT_APP_TURN_SERVER_USERNAME || '',
+      credential: process.env.REACT_APP_TURN_SERVER_PASSWORD || '',
+      urls: ['turn:vps.akhilrana.com'],
+    },
   ],
 };
